202279c7c98bce5fffcc6ee29939d9a26cdc0e5a
James R. Griffin III <griffinj@lafayette.edu>
2017-03-20 15:47:59 -0400

    Removing the deprecated template files; Aligning the styling with the Lafayette design guidelines and restructuring the landing page (#5)

--- static/js/main.js
+++ static/js/main.js
@@ -29,21 +29,6 @@
 		    }
 		});
 
-	    /**
-	     * Widgets for the collation form interface
-	     *
-	     */
-	    /*
-	    var listGroup = document.getElementById('poem-table-body');
-	    Sortable.create(listGroup, {
-		handle: '.glyphicon-move'
-	    });
-	    */
-
-	    /**
-	     * Format the data returned from the server
-	     *
-	     */
 	    function formatData (data) {
 		var markup = "" + data + "";
 
@@ -57,61 +42,5 @@
 	    function formatDataSelection (data) {
 		return data;
 	    };
-
-	    /*
-		    ajax: {
-			url: "search/poems",
-			dataType: 'json',
-			delay: 250,
-			processResults: function (data, params) {
-			    // parse the results into the format expected by Select2
-			    // since we are using custom formatting functions we do not need to
-			    // alter the remote JSON data, except to indicate that infinite
-			    // scrolling can be used
-
-			    params.page = params.page || 1;
-
-			    return {
-				results: data.items,
-				pagination: {
-				    more: (params.page * 30) < data.total_count
-				}
-			    };
-			},
-			cache: false
-		    },
-		    escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
-		    minimumInputLength: 1,
-		    templateResult: formatData, // omitted for brevity, see the source of this page
-		    templateSelection: formatDataSelection // omitted for brevity, see the source of this page
-	     */
-
-	    /**
-	     *
-	     */
-
-	    /*
-	    $.get('search/poems', function(response) {
-		    
-		    var data = JSON.parse(response);
-
-		    $.each(data.items, function(i, e) {
-			    $("#poems-select2").append('<option value="' + e + '">' + e + '</option>');
-			});
-		});
-
-	    $("#poems-select2").select2({
-		    placeholder: "Select a Poem",
-			allowClear: true,
-			}).on("change", function(event) {
-
-				var slug = $(event.target).val();
-				// Work-around due to the directory structure
-				slug = slug.replace(/\-$/, '');
-
-				// Redirect the user to the appropriate Poem
-				window.location.assign('poems/' + slug);
-			    });
-	    */
 	});
 }(jQuery));
